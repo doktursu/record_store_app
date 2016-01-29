9f08d1e6e365562daf4736be600a1b203d64536f
Sky Su <februaryskys@gmail.com>
2016-01-29 16:47:31 +0000

    Tests for finding multiple items of same attrs

--- record_store_app_spec.js
+++ record_store_app_spec.js
@@ -103,4 +103,12 @@ describe('Store', function(){
     var found = store.findItemBy('artist', 'Nosaj Thing');
     expect(found[0]).to.deep.equal(record);
   });
+  it('should be able to find multiple items', function(){
+    var record = new Record('Home', 'Nosaj Thing', 8);
+    store.addItem(record, 10);
+    var record2 = new Record('Home', 'Rudimental', 7);
+    store.addItem(record2, 1);
+    var found = store.findItemBy('name', 'Home');
+    expect(found).to.deep.equal([record, record2]);
+  });
 });

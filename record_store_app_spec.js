190642e65b72f9b020c2edb9798660fc11542b61
Sky Su <februaryskys@gmail.com>
2016-01-29 10:49:44 +0000

    Adds RecordStore constructor with name, city, and inventory

--- record_store_app_spec.js
+++ record_store_app_spec.js
@@ -1,10 +1,20 @@
 var expect = require('chai').expect;
 
 var Record = require('./record');
+var RecordStore = require('./record_store');
 
 describe('Record', function(){
   it('should have a name, artist, and price', function(){
     var record = new Record('Home', 'Nosaj Thing', 8);
     expect(record).to.deep.equal({name: 'Home', artist: 'Nosaj Thing', price: 8});
+  });
+});
+
+describe('Record Store', function(){
+  beforeEach(function createRecordStore(){
+    store = new RecordStore('Kush Records', 'Parsippany')
+  });
+  it('should have a name, city, and records inventory', function(){
+    expect(store).to.deep.equal({name: 'Kush Records', city: 'Parsippany', inventory:{}});
   });
 });
